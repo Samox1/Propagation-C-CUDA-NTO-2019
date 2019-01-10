bf483434d825397e95b9bd9dfa425bdd8988d145
Szymon Baczynski <simon.baczynski@gmail.com>
2019-01-10 00:45:21 +0000

    Update scratch.py

--- Projekt NTO - Propagacja/scratch.py
+++ Projekt NTO - Propagacja/scratch.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.misc
+import scipy.signal
 from PIL import Image
 from pathlib import Path
 import matplotlib.pyplot as plt
@@ -16,8 +17,11 @@ x = height
 y = width
 print(x,y)
 
-h_z = np.complex
-h_z = np.zeros((x,y))
+h_z_kappa = np.empty([x,y], dtype=complex)
+h_z = np.empty([x,y], dtype=complex)
+H_Z = np.empty([x,y], dtype=complex)
+u_out = np.empty([x,y], dtype=complex)
+#h_z = np.zeros((x,y))
 lam = 633*(pow(10,(-9)))
 k = 2*np.pi/lam
 z = 1000*(pow(10,(-3)))   #odlegosc symulacji
@@ -27,22 +31,36 @@ print(lam)
 print(k)
 print(z)
 
-sampling = 10*pow(10,(-6))
+sampling = 10*pow(10, (-6))
 i = 0
 o = 0
 for i in range(x):
     for o in range(y):
-        h_z[i,o]= (np.exp(1j*k*z)/(1j*lam*z))*np.exp(1j*k*(pow((i-(x/2))*sampling,2) + pow((o-(y/2))*sampling,2))/(2*z))
+        h_z_kappa[i, o] = np.exp(1j*k*(pow((i-(x/2))*sampling, 2) + pow((o-(y/2))*sampling, 2))/(2*z))
+        H_Z[i, o] = np.exp(1j*k*z) * np.exp(-1j*np.pi*lam*z*((pow(((i-(x/2))*sampling), 2) + pow(((o-(y/2))*sampling), 2))))
+h_z = h_z_kappa * (np.exp(1j * k * z) / (1j * lam * z))
 print(h_z[0][0])
-h_z_kappa = h_z / (np.exp(1j*k*z)/(1j*lam*z))
-U_fft = np.array((x,y))
+
+#u_out = scipy.signal.convolve2d(u_in, h_z)
+u_in_fft = np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(u_in)))
+#u_out = np.multiply(u_out, np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(h_z))))
+h_z = np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(h_z)))
+#u_out = u_out * h_z
+
+#u_out = np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(u_out)))
+
+
+#u_out = u_out * h_z
+#u_out = np.fft.ifft2(u_out)
+
+#U_fft = np.array((x,y))
 #u_out = np.multiply(np.fft.fft2(u_in), np.fft.fft2(h_z))
-u_out = np.multiply(np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(u_in))), h_z_kappa)
-#u_out = np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(u_in * h_z_kappa)))
+u_out = np.multiply(np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(u_in))), h_z)
+u_out = np.fft.ifftshift(np.fft.ifft2(np.fft.fftshift(u_out)))
 
 #rozwiaznie = fftshift(fft2(ifftshift()))
 
-u_out = np.fft.ifft2(u_out)
+#u_out = np.fft.ifft2(u_out)
 #u_out = np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(U_fft)))
 
 #for i in range(x):
@@ -53,16 +71,24 @@ u_out = np.fft.ifft2(u_out)
 print("KARRAMBA WYNIK")
 print(u_out[0][0])
 
-u_out = u_out.real
-u_out = u_out + abs(u_out.min())
-u_out = np.rint((u_out / u_out.max())*255)
+u_in_fft = u_in_fft.real
+u_in_fft = u_in_fft + abs(u_in_fft.min())
+u_in_fft = np.rint((u_in_fft / u_in_fft.max())*255)
+scipy.misc.imsave("u_in_fft.bmp", u_in_fft)
+
+H_Z = H_Z.real
+H_Z = H_Z + abs(H_Z.min())
+H_Z = np.rint((H_Z / H_Z.max())*255)
+scipy.misc.imsave("H_Z1.bmp", H_Z)
 
 h_z = h_z.real
 h_z = h_z + abs(h_z.min())
 h_z = np.rint((h_z / h_z.max())*255)
 scipy.misc.imsave("h_z.bmp", h_z)
-print(u_out[0][0])
 
+u_out = u_out.real
+u_out = u_out + abs(u_out.min())
+u_out = np.rint((u_out / u_out.max())*255)
 u_out_name = "u_out_size_" + str(x) + "_dist_" + str(z) + ".bmp"
 scipy.misc.imsave(u_out_name, u_out)
 #plt.imshow(u_out)
